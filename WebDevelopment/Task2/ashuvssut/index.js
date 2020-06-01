32975c2eeabe9b9764b0b099cb198baf5a54ed98
ashuvssut <ashu.khanduala@gmail.com>
2020-06-01 22:39:53 +0530

    WDev Task2 Completed

---
+++ WebDevelopment/Task2/ashuvssut/index.js
@@ -0,0 +1,10 @@
+function change(){
+    document.body.classList.toggle("dark");
+    document.getElementById("footer").classList.toggle("darker");
+    document.getElementById("theme-switcher").classList.toggle("dark");
+    document.getElementById("fa-icon").classList.toggle("fa-moon");
+    document.getElementById("fa-icon").classList.toggle("fa-sun");
+  
+}
+
+//body, .footer, .theme-switcher 
