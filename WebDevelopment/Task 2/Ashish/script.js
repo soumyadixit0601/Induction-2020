3498c3a3c9ee31f69cb7f2bc1b8f0c6ace5dc409
Ashish Behera <touchtoashish2001@gmail.com>
2020-06-04 18:37:59 +0530

    update

--- WebDevelopment/Task 2/Ashish/script.js
+++ WebDevelopment/Task 2/Ashish/script.js
@@ -1,11 +1,5 @@
-const btn = document.querySelector('.buttons');
-const body = document.querySelector('body');
 
-
-// Add Event Listener  to the button to listen for Click Events
-btn.addEventListener('click', () =>{
-    // Toggle the dark class so that the body changes its background color
-     body.classList.toggle('dark');
-     // To change the button text
-    (body.classList.contains('dark'))? btn.innerHTML =  'Light' : btn.innerHTML =  'Dark';
-});
+function thefunction() {    
+    var element = document.body;
+    element.classList.toggle("dark-mode"); 
+ }
