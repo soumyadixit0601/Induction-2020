050f4efc87da9f0edfaa85b31fe530f826d70957
ashish-max <56725030+ashish-max@users.noreply.github.com>
2020-06-04 16:28:17 +0530

    webtask2
    .

---
+++ WebDevelopment/Task 2/Ashish/script.js/const btn = document.querySelector('.buttons');.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.buttons');
+const body = document.querySelector('body');
+
+
+// Add Event Listener  to the button to listen for Click Events
+btn.addEventListener('click', () =>{
+    // Toggle the dark class so that the body changes its background color
+     body.classList.toggle('dark');
+     // To change the button text
+    (body.classList.contains('dark'))? btn.innerHTML =  'Light' : btn.innerHTML =  'Dark';
+});
+© 2020 GitHub, Inc.
